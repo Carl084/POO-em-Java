7359274416eca660e167455882b7c9fb13bd8c9e
CarlIFRN <Carloswilliam.felix@gmail.com>
2026-08-17 16:30:13 -0300

    atw exercise

---
+++ 17_08/List/Calc_avarege.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calc_avarege {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+
+        double avarege = (nota1 + nota2 + nota3) / 3;
+
+        System.out.printf("Avarege: %.2f%n", avarege);
+
+        scanner.close();
+    }
+}
